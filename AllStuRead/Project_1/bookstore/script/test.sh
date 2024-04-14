73b611de37b81e588c4ef1f1e9d85e4debe5705c
kevinyao0901 <111503954+kevinyao0901@users.noreply.github.com>
2024-04-14 21:05:24 +0800

    initiate the repository
    initiate the repository with original code & download data from BaiDuNetDisk (./bookstore/fe/data/book.db) & upload data to loacal database with script (./bookstore/script/upload_data.py)

---
+++ AllStuRead/Project_1/bookstore/script/test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export PATHONPATH=`pwd`
+coverage run --timid --branch --source fe,be --concurrency=thread -m pytest -v --ignore=fe/data
+coverage combine
+coverage report
+coverage html
+
+sleep 100
