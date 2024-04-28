ec9264632165bfd5607ec4ffc65d44264daff5bb
LittleQi <123362884+litterqi@users.noreply.github.com>
2024-04-28 11:01:51 +0800

    Update store.py

--- AllStuRead/Project_1/bookstore/be/model/store.py
+++ AllStuRead/Project_1/bookstore/be/model/store.py
@@ -1,8 +1,4 @@
-# import logging
-# import os
-# import sqlite3 as sqlite
 import pymongo
-
 
 class Store:
     client = None
@@ -14,11 +10,6 @@ class Store:
         self.init_collections()
 
     def init_collections(self):
-        # user_collection = self.database["user"]
-        # user_store_collection = self.database["user_store"]
-        # store_collection = self.database["store"]
-        # order_collection = self.database["new_order"]
-        # order_detail_collection = self.database["new_order_detail"]
         self.database["user"].create_index([("user_id", pymongo.ASCENDING)])
         self.database["user_store"].create_index([("user_id", pymongo.ASCENDING), ("store_id", pymongo.ASCENDING)])
         self.database["store"].create_index([("book_id", pymongo.ASCENDING), ("store_id", pymongo.ASCENDING)])
@@ -27,14 +18,12 @@ class Store:
         return self.database
 
 
-database_instance: Store = None
-
-
 def init_database():
-    global database_instance
-    database_instance = Store()
+    return Store()
 
 
 def get_db_conn():
     global database_instance
-    return database_instance.get_db_conn()
+    return database_instance.get_db_conn() if database_instance else init_database().get_db_conn()
+
+database_instance = init_database()
